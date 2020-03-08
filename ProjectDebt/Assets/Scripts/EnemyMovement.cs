305b28c88a7591abe789852d0782889da5ff096d
HaloSeventy7 <njmalenfant@gmail.com>
2020-03-08 14:20:04 -0400

    Added enemy movement functionality
    started wave functionality

---
+++ ProjectDebt/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 2.0f;
+
+    private Transform target;
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        if (Waypoints.waypoints.Length > 0)
+        {
+            target = Waypoints.waypoints[0];
+        }
+    }
+
+    void Update()
+    {
+        Vector2 directionOfTravel = target.position - transform.position;
+        transform.Translate(directionOfTravel.normalized * moveSpeed * Time.deltaTime, Space.World); //normalizes the movement
+
+        if (Vector2.Distance(transform.position, target.position) <= 0.2f)
+        {
+            if (currentIndex >= Waypoints.waypoints.Length - 1) //destroys the enemy when it reaches the end
+            { //TODO: Take health from player before destroying
+                Destroy(gameObject);
+            }
+            if (currentIndex < Waypoints.waypoints.Length - 1)
+            {
+                currentIndex++;
+                target = Waypoints.waypoints[currentIndex];
+            }
+        }
+
+    }
+}
---
+++ ProjectDebt/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    [SerializeField] Transform enemyPrefab;
+    [SerializeField] Transform spawnpoint;
+    [SerializeField] Text waveNumberText; //will be used to display the current wave OR timer text
+    [SerializeField] float timeBetweenWaves = 10f;
+
+    private float countdown = 2f;
+    private int waveNumber = 1;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveNumber++;
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnpoint.position, spawnpoint.rotation);
+    }
+}
